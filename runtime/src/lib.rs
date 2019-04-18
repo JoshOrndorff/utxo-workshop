38c341c4df51074d6ac68fffbc4592c7c9743ad4
nczhu <nicolezhuis@gmail.com>
2019-04-18 11:27:19 +0200

    Add: template utxo file

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -55,8 +55,7 @@ pub type BlockNumber = u64;
 /// Index of an account's extrinsic in the chain.
 pub type Nonce = u64;
 
-/// Used for the module template in `./template.rs`
-mod template;
+mod Utxo;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -188,7 +187,7 @@ impl sudo::Trait for Runtime {
 }
 
 /// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+impl Utxo::Trait for Runtime {
 	type Event = Event;
 }
 
@@ -205,8 +204,7 @@ construct_runtime!(
 		Indices: indices,
 		Balances: balances,
 		Sudo: sudo,
-		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		Utxo: utxo::{Module, Call, Storage, Event<T>},
 	}
 );
 
