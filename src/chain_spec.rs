841a0e8f2abd983e7dcfc93d961fb16609937fb8
nczhu <nicole@parity.io>
2020-04-08 16:39:20 +0800

    Fixed compiler warnings

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -7,11 +7,10 @@ use utxo_runtime::{
 use sp_consensus_aura::sr25519::{AuthorityId as AuraId};
 use grandpa_primitives::{AuthorityId as GrandpaId};
 use sc_service;
-use sp_runtime::traits::{Verify, IdentifyAccount, BlakeTwo256, Hash};
+use sp_runtime::traits::{Verify, IdentifyAccount};
 
-use primitive_types::{H256, H512};
+use primitive_types::{H256};
 use sp_core::{Pair, Public, sr25519};
-use codec::{Encode};
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::ChainSpec<GenesisConfig>;
@@ -132,7 +131,7 @@ fn testnet_genesis(initial_authorities: Vec<(AuraId, GrandpaId)>,
     println!("OUTPUT (Alice's UTXO Hash): 0x76584168d10a20084082ed80ec71e2a783abbb8dd6eb9d4893b089228498e9ff\n");
     println!("SIGSCRIPT (Alice Signature on a transaction where she spends 50 utxo on Bob): 0x6ceab99702c60b111c12c2867679c5555c00dcd4d6ab40efa01e3a65083bfb6c6f5c1ed3356d7141ec61894153b8ba7fb413bf1e990ed99ff6dee5da1b24fd83\n");
     println!("PUBKEY (Bob's public key hash): 0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48\n");
-    println!("NEW UTXO HASH in UTXOStore onchain: 0xdbc75ab8ee9b83dcbcea4695f9c42754d94e92c3c397d63b1bc627c2a2ef94e6\n";
+    println!("NEW UTXO HASH in UTXOStore onchain: 0xdbc75ab8ee9b83dcbcea4695f9c42754d94e92c3c397d63b1bc627c2a2ef94e6\n");
         
     GenesisConfig {
       system: Some(SystemConfig {
